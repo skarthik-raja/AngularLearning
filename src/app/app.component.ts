58191bd3613a6d89887004fc9064a14ac1145928
Karthik Raja <127301711+skarthik-raja@users.noreply.github.com>
2025-02-07 15:45:23 +0530

    created the angular application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
+  // template:"<h1>{{title}}</h1>",
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test';
-}
+  name = 'Kanini';
+} 

cb4248ba696f92a585e2638e8179e68e10e16b6a
Karthik Raja <127301711+skarthik-raja@users.noreply.github.com>
2025-02-11 10:46:09 +0530

    complete upto pipe conecpts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   // template:"<h1>{{title}}</h1>",
   styleUrl: './app.component.css'
@@ -22,9 +23,15 @@ export class AppComponent {
   // imageUrl = 'https://picsum.photos/200/300';
   // imageAlt = 'Random Image';
 
-  clickcount = 0;
+  // clickcount = 0;
 
-  clickme(){
-    this.clickcount++;
-  }
+  // clickme(){
+  //   this.clickcount++;
+  // }
+
+  // toDate = new Date();  
+
+  title = "Hello world";
+  Items= ['item1','item2','item3','item4','item5'];
+  price = 100;
 } 
